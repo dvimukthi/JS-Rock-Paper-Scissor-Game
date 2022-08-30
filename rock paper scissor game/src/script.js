9b6e91b72c5276d0edd2fac8f29db5b08243a223
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2022-08-30 13:08:24 +0530

    css

--- rock paper scissor game/src/script.js
+++ rock paper scissor game/src/script.js
@@ -1,5 +1,5 @@
 /*
-  Rock Paper Scissors 🚀🔥
+  Rock Paper Scissors SOLUTION 🚀🔥
   Concepts covered in this project
     👉 For loops
     👉 Dom Manipulation
@@ -14,9 +14,9 @@
 // getComputerChoice() 👉 'Rock'
 // getComputerChoice() 👉 'Scissors'
 function getComputerChoice() {
-  const rpsChoice = ["Rock", "Paper", "Scissors"];
-  const randomChoice = Math.floor(Math.random() * 3);
-  return rpsChoice[randomNumber];
+  let rpsChoices = ["Rock", "Paper", "Scissors"];
+  let computerChoice = rpsChoices[Math.floor(Math.random() * 3)];
+  return computerChoice;
 }
 
 // ** getResult compares playerChoice & computerChoice and returns the score accordingly **
@@ -25,17 +25,17 @@ function getComputerChoice() {
 // human draws - getResult('Rock', 'Rock') 👉 0
 function getResult(playerChoice, computerChoice) {
   // return the result of score based on if you won, drew, or lost
+
   let score;
+
   // All situations where human draws, set `score` to 0
-  if (playerChoice == computerChoice) {
+  if (playerChoice === computerChoice) {
     score = 0;
-    // All situations where human wins, set `score` to 1
-    // make sure to use else ifs here
-  } else if (playerChoice == "Rock" && computerChoice == "Scissors") {
+  } else if (playerChoice === "Rock" && computerChoice === "Scissors") {
     score = 1;
-  } else if (playerChoice == "Paper" && computerChoice == "Rock") {
+  } else if (playerChoice === "Paper" && computerChoice === "Rock") {
     score = 1;
-  } else if (playerChoice == "Scissors" && computerChoice == "Paper") {
+  } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
     score = 1;
 
     // Otherwise human loses (aka set score to -1)
@@ -52,29 +52,62 @@ function showResult(score, playerChoice, computerChoice) {
   // Hint: on a score of -1
   // You should do result.innerText = 'You Lose!'
   // Don't forget to grab the div with the 'result' id!
+
+  let result = document.getElementById("result");
+  switch (score) {
+    case -1:
+      result.innerText = `You Lose!`;
+      break;
+    case 0:
+      result.innerText = `It's a Draw!`;
+      break;
+    case 1:
+      result.innerText = `You Win!`;
+      break;
+  }
+
+  let playerScore = document.getElementById("player-score");
+  let hands = document.getElementById("hands");
+  playerScore.innerText = `${Number(playerScore.innerText) + score}`;
+  hands.innerText = `👱 ${playerChoice} vs 🤖 ${computerChoice}`;
 }
 
 // ** Calculate who won and show it on the screen **
 function onClickRPS(playerChoice) {
-  console.log(playerChoice);
+  const computerChoice = getComputerChoice();
+  const score = getResult(playerChoice.value, computerChoice);
+  showResult(score, playerChoice.value, computerChoice);
 }
 
 // ** Make the RPS buttons actively listen for a click and do something once a click is detected **
 function playGame() {
   // use querySelector to select all RPS Buttons
-  const rpsButtons = document.querySelectorAll(".rpsButton");
-  rpsButtons[0].onClick = () => console.log(rpsButtons[0].value);
+  let rpsButtons = document.querySelectorAll(".rpsButton");
+
   // * Adds an on click event listener to each RPS button and every time you click it, it calls the onClickRPS function with the RPS button that was last clicked *
+
   // 1. loop through the buttons using a forEach loop
   // 2. Add a 'click' event listener to each button
   // 3. Call the onClickRPS function every time someone clicks
   // 4. Make sure to pass the currently selected rps button as an argument
+
   rpsButtons.forEach((rpsButton) => {
-    rpsButton.onclick = () => onClickRPS(rpsButton.value);
+    rpsButton.onclick = () => onClickRPS(rpsButton);
   });
+
+  // Add a click listener to the end game button that runs the endGame() function on click
+  let endGameButton = document.getElementById("endGameButton");
+  endGameButton.onclick = () => endGame();
 }
 
 // ** endGame function clears all the text on the DOM **
-function endGame() {}
+function endGame() {
+  let playerScore = document.getElementById("player-score");
+  let hands = document.getElementById("hands");
+  let result = document.getElementById("result");
+  playerScore.innerText = "";
+  hands.innerText = "";
+  result.innerText = "";
+}
 
 playGame();
